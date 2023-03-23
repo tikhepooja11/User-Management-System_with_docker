d515fabea2accf6fa6d7925ebc6f7817f24f0034
Pooja <pooja.tikhe@buyerassist.io>
2023-03-23 13:54:46 +0530

    change password mongo

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,10 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-// const MONGO_USERNAME = process.env.MONGO_USERNAME || "pooja";
-// const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "pooja123";
-const MONGO_URL = `mongodb://localhost:27017/UserDB`;
-// const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.sygdhbi.mongodb.net/myFirstDatabase`;
+const MONGO_USERNAME = process.env.MONGO_USERNAME || "pooja";
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "pooja123";
+// const MONGO_URL = `mongodb://localhost:27017/UserDB`;
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.sygdhbi.mongodb.net/UserDatabase`;
 const SERVER_PORT = process.env.SERVER_port
   ? Number(process.env.SERVER_PORT)
   : 1338;
